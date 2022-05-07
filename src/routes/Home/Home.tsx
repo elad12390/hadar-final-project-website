206561333cfde67ba26058cee2a68e0b0b0a98c8
Elad Ben Haim <elad12390@gmail.com>
2022-05-07 17:23:43 +0300

    Added about page

---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,65 @@
+import {CircleCrop} from "../CircleCrop/CircleCrop";
+import {songSections} from "../../assets/data";
+import {MainContent} from "../MainContent/MainContent";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import React, {useContext, useEffect, useMemo, useState} from "react";
+import {useWindowSize} from "../../utils/hooks/useWindowSize";
+import {SongSection} from "../../models/general-models";
+import {useAsyncEffect} from "../../utils/hooks/useAsyncEffect";
+import {cacheImages} from "../../utils/hooks/useCachedImages";
+import {Store} from "../../App";
+
+export interface IVideoContentProps {
+}
+
+export const VideoContent = (props: IVideoContentProps) => {
+    const SIZE_MULTIPLIER = .6
+    const size = useWindowSize();
+    const [circleSize, setCircleSize] = useState(0);
+    const [selectedSection, setSelectedSection] = useState<{
+        section: SongSection,
+        sectionIdx: number
+    } | null>({
+        section: songSections[0],
+        sectionIdx: 0
+    });
+    const {muted, setMuted} = useContext(Store);
+
+    useAsyncEffect(async () => {
+        if (!size?.width || !size?.height) { return; }
+        setCircleSize(Math.min(size.width as number * .7, size.height as number)*SIZE_MULTIPLIER);
+    }, [size])
+
+    useEffect(() => {
+        window.addEventListener("wheel", e => e.preventDefault(), { passive:false })
+    }, [])
+
+    useMemo(() => {
+        cacheImages(songSections.map(section => section.image));
+    }, [])
+
+
+    return <>
+        <div className="image-container">
+            <CircleCrop
+                size={circleSize}
+                models={songSections}
+                onSelect={(selection, sectionIdx) => {
+                    console.log(selection, sectionIdx);
+                    setSelectedSection({
+                        section: selection,
+                        sectionIdx
+                    });
+                }}
+                selectedIdx={selectedSection?.sectionIdx}>
+                <MainContent
+                    models={songSections}
+                    onClick={(station) => setMuted(muted => !muted)}
+                    selectedIdx={selectedSection?.sectionIdx}
+                />
+                <div className={"mute"}>{muted ? <VolumeOffIcon/> : <VolumeUpIcon/>}</div>
+            </CircleCrop>
+        </div>
+    </>
+}
